d3fb42553d6d16c82c1318b04e2333d56ae97c76
Jeslyn <jeslynleon@gmail.com>
2021-07-12 00:01:54 -0700

    -adding cors -updating packages

--- Startup.cs
+++ Startup.cs
@@ -38,6 +38,7 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("Default"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
